4ed29486196c603bf8891cc121210158e3178ec6
razscu <razvan.dinculescu@outlook.com>
2018-02-24 21:13:35 +0200

    Add color to status bar again

--- app/src/main/java/com/example/raz/bistro27/MainActivity.java
+++ app/src/main/java/com/example/raz/bistro27/MainActivity.java
@@ -18,10 +18,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
         setContentView(R.layout.activity_main);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            Window w = getWindow(); // in Activity's onCreate() for instance
-            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-        }
     }
 
     public void openBrowser(View view) {
