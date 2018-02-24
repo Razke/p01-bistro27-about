934a86f2a07f54a168a4a5c06cc2000050eecede
razscu <razvan.dinculescu@outlook.com>
2018-02-24 21:04:49 +0200

    Add street address Button Change some colors.xml values Add app label back

--- app/src/main/java/com/example/raz/bistro27/MainActivity.java
+++ app/src/main/java/com/example/raz/bistro27/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.raz.bistro27;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -19,6 +22,21 @@ public class MainActivity extends AppCompatActivity {
             Window w = getWindow(); // in Activity's onCreate() for instance
             w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         }
+    }
+
+    public void openBrowser(View view) {
+
+        //Get url from tag
+        String url = (String) view.getTag();
+
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+
+        //pass the url to intent data
+        intent.setData(Uri.parse(url));
+
+        startActivity(intent);
     }
 
 }
