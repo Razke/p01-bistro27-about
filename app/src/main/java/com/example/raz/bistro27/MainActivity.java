d331192c88d6a59f99ea457e7eb4833bae0c8d0e
razscu <razvan.dinculescu@outlook.com>
2018-02-26 01:44:45 +0200

    Add the rest of the description Redo some colors for the buttons text Clean some lines of code

--- app/src/main/java/com/example/raz/bistro27/MainActivity.java
+++ app/src/main/java/com/example/raz/bistro27/MainActivity.java
@@ -50,11 +50,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("message/rfc822");
         intent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"bistro27pitesti@gmail.com"});
-
         startActivity(Intent.createChooser(intent, "Send Email"));
     }
-
-
-
 
 }
